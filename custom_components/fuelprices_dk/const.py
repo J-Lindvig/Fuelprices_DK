5138e4e03d9e71fbe76ba1d72fad924f6f49056c
J-Lindvig <54498188+J-Lindvig@users.noreply.github.com>
2022-06-16 20:31:01 +0200

    Fixed bug in filter
    There was a bug in the filter concerning the "fueltypes". This have now been fixed.

--- custom_components/fuelprices_dk/const.py
+++ custom_components/fuelprices_dk/const.py
@@ -1,11 +1,11 @@
 CONF_CLIENT = "client"
 CONF_FUELCOMPANIES = "companies"
-CONF_FUELTYPES = "gas"
+CONF_FUELTYPES = "fueltypes"
 CONF_PLATFORM = "sensor"
 CREDITS = [
-	{"Created by": "J-Lindvig (https://github.com/J-Lindvig)"},
-	{"Contributor": "Johan Leth (https://github.com/JVLeth)"},
-	{"Techinal support": "Peer Jensen (www.fuelfinder.dk)"},
+    {"Created by": "J-Lindvig (https://github.com/J-Lindvig)"},
+    {"Contributor": "Johan Leth (https://github.com/JVLeth)"},
+    {"Techinal support": "Peer Jensen (www.fuelfinder.dk)"},
 ]
 DOMAIN = "fuelprices_dk"
 PATH = "./custom_components/" + DOMAIN + "/"
